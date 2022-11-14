d058258ac734cf6720d9d4255cd0cb1c1efb5684
Catalina Astudillo <Catalina.astudillo7@gmail.com>
2022-11-14 16:21:09 -0300

    Lista arreglo

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import Main from './componente/Main/Main';
 
 
 function App() {
+  const array = [
+    {id: 1, nombre: 'Carlos', edad: 28, carrera: 'Diseño Garfico'},
+    {id: 2, nombre: 'Josefa', edad: 30, carrera: 'Moda'},
+    {id: 3, nombre: 'Marian', edad: 24, carrera: 'Arquitectura'},
+    {id: 4, nombre: 'Tomas', edad: 18, carrera: 'Ingeniero'},
+  ]
   return (
     <>
       <Header/>
-      <Main/>
+      <Main array = {array}/>
       <Footer/>    
       
     </>
--- src/componente/Main/Main.js
+++ src/componente/Main/Main.js
@@ -3,12 +3,19 @@ import React from 'react';
 import '../Main/Main.css';
 import Titulo from '../Titulo/Titulo';
 
-const Main = () => {
+const Main = ({array}) => {
     return (
         <main className="contenedor-main">
             <div>
                 <Titulo titulo = 'hola mundo'/>
             </div>
+            <ul>
+                {array.map((arreglo) => {
+                    return (
+                        <li >{arreglo.nombre}</li>
+                    )
+                })}
+            </ul>
         </main>
     )
 }
